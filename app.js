730cb6bc14e255cc20998e9a6d183ec887f726b6
jimbo23 <kiefersoon@gmail.com>
2022-06-04 00:00:01 +0800

    configure for ES6 syntax

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const app = express()
+import express from "express";
 
-const PORT = 3000
+const app = express();
 
-app.listen(PORT, ()=>console.log(`listen to ${PORT}`))
+const PORT = 3000;
 
-app.get('/', (req,res) => {
-	res.send("Hello World")
-})
+app.listen(PORT, () => console.log(`listen to ${PORT}`));
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
